a85dde2098f050233330bf88c15a478c080334e3
Thomas Van Gysegem <thomasvangysegem@gmail.com>
2019-10-14 13:58:32 +0200

    [add] Starting realm set from the level

--- nodes/Level/Level.gd
+++ nodes/Level/Level.gd
@@ -3,3 +3,32 @@ class_name Level
 """
 Base for every level
 """
+
+# On which plan the level starts
+enum Realms {REAL, SHADOW}
+export (Realms) var start_realm
+
+var node_realms: Dictionary
+var current_realm: Node
+
+
+func _ready():
+	node_realms = {
+		Realms.REAL: $RealWorld,
+		Realms.SHADOW: $ShadowRealm
+	}
+	
+	for realm in node_realms.values():
+		realm.visible = false
+	
+	change_plan(node_realms[start_realm])
+
+
+func change_plan(realm: Node):
+	print(realm)
+	if current_realm:
+		current_realm.visible = false
+	
+	current_realm = realm
+	
+	current_realm.visible = true
