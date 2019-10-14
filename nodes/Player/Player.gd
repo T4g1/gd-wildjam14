0442337b6c9a17615879b84bb84790a69fabcbdf
Thomas Van Gysegem <thomasvangysegem@gmail.com>
2019-10-14 11:40:57 +0200

    [enh] Removed some debug

--- nodes/Player/Player.gd
+++ nodes/Player/Player.gd
@@ -9,7 +9,3 @@ func _input(event):
 	if event.is_action_pressed("ui_accept") and not in_dialog:
 		Utils.get_character("Moriarty's Shadow").continue_story()
 		in_dialog = true
-	
-	if event.is_action_pressed("ui_left") and not in_dialog:
-		Utils.get_character("Jonh").continue_story()
-		in_dialog = true
