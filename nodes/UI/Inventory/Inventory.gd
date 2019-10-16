13d231bc3eb6930b9f84a0428a64f500d6980c0d
Thomas Van Gysegem <thomasvangysegem@gmail.com>
2019-10-16 23:41:22 +0200

    [add] Preview position for inventory

--- nodes/UI/Inventory/Inventory.gd
+++ nodes/UI/Inventory/Inventory.gd
@@ -10,9 +10,12 @@ export (int) var offset = 25
 
 var displayed_position: Vector2
 var hidden_position: Vector2
+var preview_position: Vector2
 var hidded = true
 
-onready var background = $MarginContainer/Background
+onready var background = $Background
+onready var action_arrow = $ActionArrow
+onready var action_tween = $ActionArrow/Tween
 
 
 func _ready():
@@ -20,6 +23,9 @@ func _ready():
 	
 	hidden_position = Vector2(0, 0)
 	hidden_position.x = offset - background.rect_size.x
+	
+	preview_position = hidden_position
+	preview_position.x += 10
 	
 	rect_position = hidden_position
 	hidded = true
@@ -32,6 +38,9 @@ func _unhandled_event(event):
 
 func _on_hide():
 	hidded = true
+	
+	action_arrow.rect_rotation = 0
+	
 	$Tween.stop_all()
 	$Tween.interpolate_property(self, "rect_position", rect_position, hidden_position, hidding_speed, Tween.TRANS_LINEAR, 0)
 	$Tween.start()
@@ -39,9 +48,28 @@ func _on_hide():
 
 func _on_display():
 	hidded = false
+	
+	action_arrow.rect_rotation = 180
+	
 	$Tween.stop_all()
 	$Tween.interpolate_property(self, "rect_position", rect_position, displayed_position, apparition_speed, Tween.TRANS_BOUNCE, Tween.EASE_OUT)
 	$Tween.start()
+
+
+func _on_hover():
+	if hidded:
+		action_tween.interpolate_property(self, "rect_position", hidden_position, preview_position, 0.2, Tween.TRANS_LINEAR, 0)
+	
+	action_tween.interpolate_property(action_arrow, "rect_scale", Vector2(0.2, 0.2), Vector2(1.2, 1.2), 0.5, Tween.TRANS_LINEAR, 0)
+	action_tween.start()
+
+
+func _on_exit():
+	if hidded:
+		rect_position = hidden_position
+	
+	action_arrow.rect_scale = Vector2(1, 1)
+	action_tween.stop_all()
 
 
 func _on_input(event):
