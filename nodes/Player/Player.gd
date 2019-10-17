2b8e93afcff2c05b2fb94838fe2ab58120be7e9e
Thomas Van Gysegem <thomasvangysegem@gmail.com>
2019-10-17 18:03:28 +0200

    [enh] Talk button for characters

--- nodes/Player/Player.gd
+++ nodes/Player/Player.gd
@@ -12,7 +12,3 @@ func _ready():
 func _input(event):
 	if in_dialog:
 		return
-	
-	# TODO: Talk to other should be handled by click
-	if event.is_action_pressed("ui_cancel"):
-		Utils.get_character("Moriarty's Shadow").continue_story()
